1be353dec078c6999c9eba7e389b96cd3a5a031d
josephwon0310 <josephwon0310@gmail.com>
2020-01-26 22:39:19 -0500

    tableview good

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Joseph Won on 1/26/20.
+//  Copyright © 2020 Joseph Won. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
