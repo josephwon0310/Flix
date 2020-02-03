1635f91a72c47dcae41a7c7232cd3fd16bdf1830
josephwon0310 <josephwon0310@gmail.com>
2020-02-02 20:04:40 -0500

    unit 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Joseph Won on 2/2/20.
+//  Copyright © 2020 Joseph Won. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
